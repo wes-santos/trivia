120e1b240ea998ab72c73ff5c718dd55ffefb4ca
wes-santos <weslleyalmerindo2@gmail.com>
2022-07-01 13:21:47 -0300

    Updates

---
+++ precache-manifest.23065ef26aca51cf79c61001f36c412c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b919fb8b879a64ec5f94a1c7f94fa6d8",
+    "url": "/trivia/index.html"
+  },
+  {
+    "revision": "361779217c689d68b87a",
+    "url": "/trivia/static/css/main.7a152120.chunk.css"
+  },
+  {
+    "revision": "3cf35e7efbbb2f3870ee",
+    "url": "/trivia/static/js/2.c2cb5c4d.chunk.js"
+  },
+  {
+    "revision": "380b0caed5f6edef4aeca953b95c2eb4",
+    "url": "/trivia/static/js/2.c2cb5c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361779217c689d68b87a",
+    "url": "/trivia/static/js/main.509e2736.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/trivia/static/js/main.509e2736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf3ddda99dc054ff6df",
+    "url": "/trivia/static/js/runtime-main.55dca74b.js"
+  },
+  {
+    "revision": "67311dc7369f74145da9dfbdf2f8e21a",
+    "url": "/trivia/static/media/ampulheta.67311dc7.gif"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia/static/media/trivia.466d153e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8dc46495604c6dd94366cc413aa8e564.js"
+  "/trivia/precache-manifest.23065ef26aca51cf79c61001f36c412c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trivia/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
